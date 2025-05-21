57d39ed895f6c20f9cd65e3925904d2fae21364d
Renz Cole <renz2arjhay@gmail.com>
2025-05-22 07:51:55 +0800

    feat: enhance header component with navigation links and state management

--- client/src/components/header.tsx
+++ client/src/components/header.tsx
@@ -1,12 +1,32 @@
 import React from "react";
 import { Button } from "./ui/button";
+import { Link } from "react-router-dom";
+import { useMainStore } from "../store";
 
 const Header: React.FC = () => {
+  const page = useMainStore((state) => state.page);
+
   return (
-    <div className="w-full flex justify-between items-center px-4 py-2 bg-gray-900 shadow-2xl">
+    <div className="w-full flex justify-between items-center px-4 py-2 bg-[#0E2148] shadow-2xl">
       <div className="Nunito-Extra-Bold text-xl text-white">
         QR Code Attendance System
       </div>
+
+      <div className="text-white Nunito-Medium flex gap-4">
+        <Link
+          to="/"
+          className={`${page === "home" && "border-b-2 border-[#E3D095]"}`}
+        >
+          Home
+        </Link>
+        <Link
+          to="/students"
+          className={`${page === "students" && "border-b-2 border-[#E3D095]"}`}
+        >
+          Students
+        </Link>
+      </div>
+
       <div>
         <Button variant="secondary" size="sm">
           Logout
