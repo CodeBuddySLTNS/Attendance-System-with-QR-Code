a6a4f5d1019880ab53bad959086654195b2d4e39
Renz Cole <renz2arjhay@gmail.com>
2025-05-22 20:41:32 +0800

    feat: add background image layer component with logo

---
+++ client/src/components/bg-image-layer.tsx
@@ -0,0 +1,15 @@
+import bgImage from "/images/paclogo.png";
+
+const BgImageLayer = () => {
+  return (
+    <div className="absolute inset-0 z-[0] flex items-center justify-center bg-opacity-30">
+      <img
+        src={bgImage}
+        alt="Background"
+        className="opacity-[11%] w-[300px] md:w-[400px]"
+      />
+    </div>
+  );
+};
+
+export default BgImageLayer;
