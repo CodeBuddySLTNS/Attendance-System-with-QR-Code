05b5ef515e5ad5b10f0af1abbc92d215efddb281
Renz Cole <renz2arjhay@gmail.com>
2025-05-23 11:32:39 +0800

    feat: enhance landing page with attendance management integration, real-time clock display, and improved QR code scanning functionality

--- client/src/pages/landing-page.tsx
+++ client/src/pages/landing-page.tsx
@@ -1,12 +1,45 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Card } from "../components/ui/card";
 import { Scanner, type IDetectedBarcode } from "@yudiel/react-qr-scanner";
 import PresentStudentsTabe from "../components/present-students-table";
-import type { PresentStudent } from "../types/students.types";
 import BgImageLayer from "@/components/bg-image-layer";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import { coleAPI } from "@/lib/utils";
+import { toast } from "sonner";
+import type { AxiosError } from "axios";
+
+interface TimeData {
+  hours: string;
+  minutes: string;
+  seconds: string;
+  amPm: string;
+}
 
 const LandingPage: React.FC = () => {
-  const [presentStudents, setPresentStudents] = useState<PresentStudent[]>([]);
+  const queryClient = useQueryClient();
+  const [time, setTime] = useState<TimeData>({
+    hours: "00",
+    minutes: "00",
+    seconds: "00",
+    amPm: "AM",
+  });
+
+  const { data: attendances } = useQuery({
+    queryKey: ["attendances"],
+    queryFn: coleAPI(
+      "/attendances?date=" + new Date().toISOString().split("T")[0]
+    ),
+  });
+
+  const { mutateAsync: addAttendance } = useMutation({
+    mutationFn: coleAPI("/attendances/add", "POST"),
+    onError: (e: AxiosError<{ message?: string }>) => {
+      if (e.response?.data?.message === "Already exists!") {
+        return toast.error("Attendance already exists!");
+      }
+      toast.error("Failed to add attendance");
+    },
+  });
 
   const handleScanResult = async (result: IDetectedBarcode[]) => {
     try {
@@ -14,24 +47,45 @@ const LandingPage: React.FC = () => {
       if (!data.userId || !data?.name || !data?.courseAndYear)
         throw new Error("Invalid Format!");
 
-      const presentValue: PresentStudent = {
-        type: "in",
-        userId: data.userId,
-        name: data.name,
-        courseAndYear: data.courseAndYear,
-        date: new Date(),
-      };
+      try {
+        await addAttendance({
+          type: "in",
+          userId: data.userId,
+          dateTime: new Date()
+            .toLocaleString("sv-SE", { timeZone: "Asia/Manila" })
+            .replace("T", " "),
+          date: new Date(),
+        });
 
-      setPresentStudents((prev) => [...prev, presentValue]);
+        queryClient.invalidateQueries({ queryKey: ["attendances"] });
+      } catch (error) {
+        if (error instanceof Error) throw new Error(error.message);
+      }
     } catch (error) {
-      console.log(error);
+      console.error(error);
     }
   };
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      const currentTime = new Date();
+      const timeData = {
+        hours: (currentTime.getHours() % 12 || 12).toString().padStart(2, "0"),
+        minutes: currentTime.getMinutes().toString().padStart(2, "0"),
+        seconds: currentTime.getSeconds().toString().padStart(2, "0"),
+        amPm: currentTime.getHours() >= 12 ? "PM" : "AM",
+      };
+
+      setTime(timeData);
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, []);
 
   return (
     <div className="w-full h-full p-8 flex justify-center items-center bg-gray-400 shadow-xl">
       <Card className="w-full h-full p-4 grid grid-cols-[1fr_2fr] rounded-lg bg-gray-100">
-        <Card className="p-4 rounded-lg ">
+        <Card className="p-4 rounded-lg grid grid-cols-1 grid-rows-[max-content_1fr] gap-2">
           <div className="flex flex-col gap-1.5">
             <h2 className="font-bold text-xl text-center">
               Scan your QR Code here for your attendance
@@ -44,10 +98,15 @@ const LandingPage: React.FC = () => {
               }}
             />
           </div>
+          <div className="h-full flex items-center justify-center gap-2 p-1 text-2xl Nunito-Bold border rounded shadow-md">
+            <span>{time.hours}</span>:<span>{time.minutes}</span>:
+            <span>{time.seconds}</span>
+            <span>{time.amPm}</span>
+          </div>
         </Card>
         <Card className="p-4 rounded-lg gap-1 overflow-hidden relative">
           <BgImageLayer />
-          <PresentStudentsTabe data={presentStudents} />
+          <PresentStudentsTabe data={attendances || []} />
         </Card>
       </Card>
     </div>
