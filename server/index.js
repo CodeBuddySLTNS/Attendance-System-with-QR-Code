7f422e863d19fc03b308ad14331d96875cc009f7
Renz Cole <renz2arjhay@gmail.com>
2025-06-30 20:52:43 +0800

    refactor: comment out logging middleware for cleaner request handling

--- server/index.js
+++ server/index.js
@@ -13,7 +13,7 @@ const PORT = 5000;
 
 app.use(express.json());
 app.use(cors());
-app.use((req, res, next) => (console.log(req.path, req.method), next()));
+// app.use((req, res, next) => (console.log(req.path, req.method), next()));
 app.use(authenticate);
 
 app.use("/login", auth);
