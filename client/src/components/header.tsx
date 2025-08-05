184a28a8ecdceb842d374f26c3fd563f3f751ebf
Renz Cole <renz2arjhay@gmail.com>
2025-08-05 10:30:38 +0800

    refactor: reorganize Header component structure for improved readability and maintainability

--- client/src/components/header.tsx
+++ client/src/components/header.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import { Button } from "./ui/button";
 import { Link } from "react-router-dom";
 import { useMainStore } from "../store";
-import Login from "./login";
 
 const Header: React.FC = () => {
   const page = useMainStore((state) => state.page);
@@ -14,51 +13,51 @@ const Header: React.FC = () => {
   };
 
   return (
-    <div className="w-full flex justify-between items-center px-4 py-2.5 z-10 bg-[#0E2148] shadow-2xl">
-      <div className="Nunito-Extra-Bold flex items-center gap-2 text-xl text-white">
-        <div className="w-8 h-8">
-          <img src="/images/paclogo.png" />
+    loggedIn && (
+      <div className="w-full flex justify-between items-center px-4 py-2.5 z-10 bg-[#0E2148] shadow-2xl">
+        <div className="Nunito-Extra-Bold flex items-center gap-2 text-xl text-white">
+          <div className="w-8 h-8">
+            <img src="/images/paclogo.png" />
+          </div>
+          <span>QR Code Attendance System</span>
         </div>
-        <span>QR Code Attendance System</span>
-      </div>
 
-      <div
-        className={`${
-          !loggedIn && "hidden"
-        } text-white Nunito-Medium flex gap-4`}
-      >
-        <Link
-          to="/"
-          className={`${page === "home" && "border-b-2 border-[#E3D095]"}`}
-        >
-          Home
-        </Link>
-        <Link
-          to="/students"
-          className={`${page === "students" && "border-b-2 border-[#E3D095]"}`}
-        >
-          Students
-        </Link>
-        <Link
-          to="/attendance-records"
+        <div
           className={`${
-            page === "attendance-records" && "border-b-2 border-[#E3D095]"
-          }`}
+            !loggedIn && "hidden"
+          } text-white Nunito-Medium flex gap-4`}
         >
-          Attendance Records
-        </Link>
-      </div>
+          <Link
+            to="/"
+            className={`${page === "home" && "border-b-2 border-[#E3D095]"}`}
+          >
+            Home
+          </Link>
+          <Link
+            to="/students"
+            className={`${
+              page === "students" && "border-b-2 border-[#E3D095]"
+            }`}
+          >
+            Students
+          </Link>
+          <Link
+            to="/attendance-records"
+            className={`${
+              page === "attendance-records" && "border-b-2 border-[#E3D095]"
+            }`}
+          >
+            Attendance Records
+          </Link>
+        </div>
 
-      <div>
-        {loggedIn ? (
+        <div>
           <Button variant="secondary" size="sm" onClick={handleLogout}>
             Logout
           </Button>
-        ) : (
-          <Login />
-        )}
+        </div>
       </div>
-    </div>
+    )
   );
 };
 
