66ba63aaa86719457268e41a0d9e73b302b4d7b5
Renz Cole <renz2arjhay@gmail.com>
2025-08-05 19:12:08 +0800

    feat: add user state management and update routing logic in App component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect } from "react";
-import { Route, Routes, useNavigate } from "react-router-dom";
+import { Route, Routes, useLocation, useNavigate } from "react-router-dom";
 import AttendanceRecords from "./pages/attendance-records";
 import Layout from "./layout";
 import StudentsPage from "./pages/students-page";
@@ -10,9 +10,12 @@ import { useMainStore } from "./store";
 import Login from "./pages/auth/login";
 import LandingPage from "./pages/landing-page";
 import Signup from "./pages/auth/signup";
+import ViewClass from "./pages/teacher/view-class";
 
 const App: React.FC = () => {
   const navigate = useNavigate();
+  const location = useLocation();
+  const user = useMainStore((state) => state.user);
   const { data, isLoading } = useQuery({
     queryKey: ["session"],
     queryFn: coleAPI("/login/session"),
@@ -21,11 +24,20 @@ const App: React.FC = () => {
   useEffect(() => {
     if (data) {
       useMainStore.getState().setLoggedIn(true);
+      useMainStore.getState().setUser(data);
     }
-    if (!isLoading && !data) {
+
+    if (!isLoading && !data && !user) {
       navigate("/login");
     }
-  }, [data, navigate, isLoading]);
+  }, [data, navigate, isLoading, user]);
+
+  useEffect(() => {
+    // switch (location.pathname) {
+    //   case "/login":
+    //     if (user) navigate("/");
+    // }
+  }, [location.pathname, user, navigate]);
 
   return (
     <Layout>
@@ -33,6 +45,7 @@ const App: React.FC = () => {
         <Route path="/" element={<LandingPage />} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
+        <Route path="/class/:classId" element={<ViewClass />} />
         <Route path="/students" element={<StudentsPage />} />
         <Route path="/attendance-records" element={<AttendanceRecords />} />
       </Routes>
