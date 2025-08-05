3166494d2fbb5c6443fdb228cedfd651f3bb37dc
Renz Cole <renz2arjhay@gmail.com>
2025-08-05 19:12:20 +0800

    refactor: replace isTeacher state with user object in main store

--- client/src/store.ts
+++ client/src/store.ts
@@ -1,21 +1,22 @@
 import { create } from "zustand";
+import type { User } from "./types/students.types";
 
 interface MainStore {
   page: string;
   loggedIn: boolean;
-  isTeacher: boolean;
+  user: User | null;
 
   setPage: (page: string) => void;
   setLoggedIn: (status: boolean) => void;
-  setTeacher: (status: boolean) => void;
+  setUser: (status: User | null) => void;
 }
 
 export const useMainStore = create<MainStore>((set) => ({
   page: "home",
   loggedIn: false,
-  isTeacher: false,
+  user: null,
 
   setPage: (page) => set({ page }),
   setLoggedIn: (status) => set({ loggedIn: status }),
-  setTeacher: (status) => set({ loggedIn: status }),
+  setUser: (user) => set({ user }),
 }));
