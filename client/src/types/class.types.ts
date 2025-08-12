5ec5dc3f7cb3de0d472759dd840b0da890fb36f0
Renz Cole <renz2arjhay@gmail.com>
2025-08-12 20:06:56 +0800

    changed file extensions from tsx to ts

---
+++ client/src/types/class.types.ts
@@ -0,0 +1,7 @@
+export interface ClassData {
+  className: string;
+  departmentId: string;
+  department: string;
+  year: number;
+  time: string;
+}
---
+++ client/src/types/students.types.ts
@@ -0,0 +1,48 @@
+export interface PresentStudent {
+  type?: string;
+  userId: number;
+  name: string;
+  courseAndYear: string;
+  date: Date;
+}
+
+export interface Student {
+  userId: number;
+  studentId: number;
+  name: string;
+  departmentName: string;
+  departmentAcronym: string;
+  year: number;
+}
+
+export interface StudentByDepartment {
+  userId: number;
+  name: string;
+  studentId: number | null;
+  isPresent: boolean;
+  attendance: {
+    attendanceId: number;
+    type: string;
+    dateTime: string;
+    date: string;
+  };
+}
+
+export interface AttendanceRecords {
+  departmentName: string;
+  year: number;
+  totalStudents: number;
+  presentCount: number;
+  absentCount: number;
+  students: StudentByDepartment[];
+}
+
+export interface User {
+  userId: number;
+  name: string;
+  role: "student" | "teacher" | "admin";
+  departmentName?: string;
+  departmentAcronym?: string;
+  year?: number;
+  studentId?: number | null;
+}
